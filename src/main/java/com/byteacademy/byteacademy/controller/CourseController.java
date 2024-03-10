dded62ab6dde56cbaa4b1e954bc03936cfdc5684
Emil K <karimoff676@gmail.com>
2024-03-10 04:04:46 +0400

    add

---
+++ src/main/java/com/byteacademy/byteacademy/controller/CourseController.java
@@ -0,0 +1,46 @@
+package com.byteacademy.byteacademy.controller;
+
+import com.byteacademy.byteacademy.model.course.request.CourseDTO;
+import com.byteacademy.byteacademy.model.course.response.ClientCourseFullDTO;
+import com.byteacademy.byteacademy.model.course.response.ClientCourseListDTO;
+import com.byteacademy.byteacademy.service.interfaces.CourseService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("${root.url}/course")
+@RequiredArgsConstructor
+public class CourseController {
+    private final CourseService courseService;
+
+    @GetMapping
+    public Page<ClientCourseListDTO> getAllList(Pageable pageable){
+        return courseService.getAllList(pageable);
+    }
+
+    @GetMapping("/{slug}")
+    public ClientCourseFullDTO getCourseBySlug(@PathVariable String slug){
+        return courseService.getCourseBySlug(slug);
+    }
+
+    @PostMapping
+    public void add(@RequestBody @Valid CourseDTO courseDTO){
+        courseService.add(courseDTO);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void update(@PathVariable Long id, @RequestBody CourseDTO courseDTO){
+        courseService.update(id, courseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        courseService.delete(id);
+    }
+}
